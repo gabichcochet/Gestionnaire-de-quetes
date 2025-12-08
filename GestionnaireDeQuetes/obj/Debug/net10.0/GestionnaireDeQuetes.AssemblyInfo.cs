50a837d5a45b0dd9a5cde4beaeeaa11593d93364
cocgabriel <gabriel.cochet@ynov.com>
2025-12-08 09:27:20 +0100

    correction

--- GestionnaireDeQuetes/obj/Debug/net10.0/GestionnaireDeQuetes.AssemblyInfo.cs
+++ GestionnaireDeQuetes/obj/Debug/net10.0/GestionnaireDeQuetes.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GestionnaireDeQuetes")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5d88fa5a6d736ead53ead9c26841e790d6df00ba")]
 [assembly: System.Reflection.AssemblyProductAttribute("GestionnaireDeQuetes")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GestionnaireDeQuetes")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
