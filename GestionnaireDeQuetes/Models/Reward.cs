71414c065aefabb8708c49f4a54ed18fa59bbf57
kwilliam <william.krommer@ynov.com>
2025-12-14 11:16:28 +0100

    Reward

--- GestionnaireDeQuetes/Models/Reward.cs
+++ GestionnaireDeQuetes/Models/Reward.cs
@@ -1,8 +1,20 @@
-namespace Game.Rewards
+namespace GestionnaireDeQuetes.Models
 {
     public class Reward
     {
-        public string Description { get; set; } = string.Empty;
+        public string Description { get; set; }
+        
         public int Value { get; set; }
+
+        public Reward(string description, int value)
+        {
+            Description = description;
+            Value = value;
+        }
+
+        public override string ToString()
+        {
+            return $"{Description} (Valeur: {Value})";
+        }
     }
 }
